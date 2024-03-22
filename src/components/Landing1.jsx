341b0d61728ae8af42e5c56aa5543b32f011ac4b
idhika22 <133572308+idhika22@users.noreply.github.com>
2024-03-22 18:52:44 +0530

    360 cards idhiks

--- src/components/Landing1.jsx
+++ src/components/Landing1.jsx
@@ -4,7 +4,23 @@ import ExploreArchive from "../components/ExploreArchive";
 import { useState } from "react";
 
 function Landing1() {
-  return <></>;
+  return <>
+  <div className={Classes.pageBG}>
+        <div className={Classes.pageContainer}>
+          <div className={Classes.textContainer}>
+            <div className={Classes.welcome}>WELCOME TO ARCHIVE</div>
+            <div className={Classes.desc}>
+              WE ARCHIVE AND DIGITALIZE ALL KINDS OF ANCIENT MANUSCRIPTS AND
+              SCULPTURES FOR REASERCH AND PUBLIC DISPLAY.
+            </div>
+            <div className={Classes.archiveButton}>
+              EXPLORE ARCHIVE
+            </div>
+          </div>
+          <img src={welcomeStatue} className={Classes.welcomeStatue} />
+        </div>
+      </div>
+  </>;
 }
 
 export default Landing1;
--- src/components/ThreeSixtyCard.jsx
+++ src/components/ThreeSixtyCard.jsx
@@ -3,14 +3,29 @@ import classes from "../components/ThreeSixtyCard.module.css";
 function ThreeSixtyCard() {
     return(<div className={classes.pageBG
     }><div className={classes.pageContainer}>
-        <div></div>
-        
-        
-        
-        
-        
-        
-        
+        <div className={classes.flexcontainer}>
+        <div className={classes.flexbox11}>
+           <h1 className={classes.text}>360 Virtual Tour</h1> 
+        </div>
+        </div>
+        <div className={classes.flexcontainer1}>
+        <div className={classes.flexbox1}>
+        <h1 className={classes.text}>kapaleeshwarar</h1> 
+        <h1 className={classes.text}>Temple</h1> 
+        </div>
+        <div className={classes.flexbox2}>
+           <h1 className={classes.text}>Sri ViaJaya Ganapthi </h1> 
+           <h1 className={classes.text}>Temple</h1> 
+        </div>
+        <div className={classes.flexbox3}>
+           <h1 className={classes.text}>Vaikundaperumal</h1> 
+           <h1 className={classes.text}>Temple</h1> 
+        </div>
+        <div className={classes.flexbox4}>
+           <h1 className={classes.text}>Kailasanathar</h1> 
+           <h1 className={classes.text}>Temple</h1> 
+        </div>
+        </div>
         </div></div>)
 }
 
